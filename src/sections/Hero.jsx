7d6c38013664bb2165739cff3916194136c7ba55
Yuto Gushiken <yuyuyu11301213@gmail.com>
2025-12-28 20:50:08 +0900

    test: auto deploy check

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,14 +5,6 @@ export default function Hero() {
     <section className="hero">
       <div className="hero-bg" />
 
-      {/* 空気 */}
-      <img src="/images/hero-sub-1.png" className="hero-sub s1" alt="" />
-      <img src="/images/hero-sub-2.png" className="hero-sub s2" alt="" />
-      <img src="/images/hero-sub-3.png" className="hero-sub s3" alt="" />
-
-      {/* 主役 */}
-      <img src="/images/hero-main.png" className="hero-main" alt="" />
-
       {/* ロゴ */}
       <h1 className="hero-logo">LILU</h1>
     </section>
